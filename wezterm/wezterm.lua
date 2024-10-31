2d80fbde2ddf64e80c8b4a1bb4b99aa2c4629120
SahilMahale <mahalesahil@gmail.com>
2024-10-31 20:17:22 +0530

    Hypr,kitty

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -10,8 +10,8 @@ config.automatically_reload_config = true
 config.color_scheme = "Rosé Pine (Gogh)"
 config.window_background_opacity = 0.99
 config.font = wezterm.font_with_fallback({
-	{ family = "JetBrains Mono", weight = "Regular", style = "Normal" },
-	{ family = "Symbols Nerd Font Mono", scale = 0.75 },
+  { family = "JetBrains Mono",         weight = "Regular", style = "Normal" },
+  { family = "Symbols Nerd Font Mono", scale = 0.75 },
 })
 --config.font = wezterm.font("IosevkaTerm NFM", { italic = false })
 config.font_size = 14
@@ -19,31 +19,31 @@ config.cell_width = 1
 
 --Tab styff
 config.keys = {
-	{ key = "-", mods = "ALT", action = act.ActivateTabRelative(-1) },
-	{ key = "=", mods = "ALT", action = act.ActivateTabRelative(1) },
-	{
-		key = "w",
-		mods = "CTRL",
-		action = wezterm.action.CloseCurrentTab({ confirm = true }),
-	},
+  { key = "-", mods = "ALT", action = act.ActivateTabRelative(-1) },
+  { key = "=", mods = "ALT", action = act.ActivateTabRelative(1) },
+  {
+    key = "w",
+    mods = "CTRL",
+    action = wezterm.action.CloseCurrentTab({ confirm = true }),
+  },
 }
 for i = 1, 8 do
-	-- CTRL+ number to activate that tab
-	table.insert(config.keys, {
-		key = tostring(i),
-		mods = "CTRL",
-		action = act.ActivateTab(i - 1),
-	})
-	-- F1 through F8 to activate that tab
-	table.insert(config.keys, {
-		key = "F" .. tostring(i),
-		action = act.ActivateTab(i - 1),
-	})
+  -- CTRL+ number to activate that tab
+  table.insert(config.keys, {
+    key = tostring(i),
+    mods = "CTRL",
+    action = act.ActivateTab(i - 1),
+  })
+  -- F1 through F8 to activate that tab
+  table.insert(config.keys, {
+    key = "F" .. tostring(i),
+    action = act.ActivateTab(i - 1),
+  })
 end
 
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
-config.max_fps = 240
+-- config.max_fps = 240
 config.window_decorations = "NONE"
 -- for windows wsl launch
 -- config.default_domain = 'WSL:Ubuntu-20.04'
