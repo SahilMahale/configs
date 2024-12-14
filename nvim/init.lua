a6118758dd48299d23a6365441ea66456368227b
SahilMahale <mahalesahil@gmail.com>
2024-12-14 18:16:02 +0530

    kitty backgroud + nvim transparent bg + hyprlandConf

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,12 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("plugins.user")
+--  to make the background transparent
+vim.cmd([[
+augroup user_colors
+  autocmd!
+  autocmd ColorScheme * highlight Normal ctermbg=NONE guibg=NONE
+augroup END
+]])
 vim.cmd("colorscheme kanagawa-wave")
 vim.o.termguicolors = true
