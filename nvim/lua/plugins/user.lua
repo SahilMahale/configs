5acc67abab7566196b3db34a381d1b89760beed3
SahilMahale <mahalesahil@gmail.com>
2024-12-21 15:21:04 +0530

    making kanagawa conf in lazyvim style

--- nvim/lua/plugins/user.lua
+++ nvim/lua/plugins/user.lua
@@ -10,15 +10,17 @@
 -- * override the configuration of LazyVim plugins
 return {
   --add Kanagawa
-  {"rebelot/kanagawa.nvim",name = "Kanagawa",priority = 1000,
-    config=function ()
-    require('kanagawa').setup({
+  {"rebelot/kanagawa.nvim",
+    name = "Kanagawa",
+    lazy=false,
+    priority = 1000,
+    opts = {
     compile = false,             -- enable compiling the colorscheme
     undercurl = true,            -- enable undercurls
     commentStyle = { italic = true },
-    functionStyle = { bold=true },
+    functionStyle = {  },
     keywordStyle = { italic = true, bold = true },
-    statementStyle = { bold = true },
+    statementStyle = {  },
     typeStyle = {},
     transparent = true,         -- do set background color
     dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
@@ -27,20 +29,19 @@ return {
         palette = {},
         theme = { wave = {}, lotus = {}, dragon = {}, all = {
          syn = {
-              keyword='#D27E99' -- sakuraPink from kanagawa_color.lua the official
-            } 
+              keyword='#D27E99', -- sakuraPink from kanagawa_color.lua the official
+            }
           } },
     },
-    overrides = function(colors) -- add/modify highlights
+    --[[ overrides = function(colors) -- add/modify highlights
         return {}
-    end,
+    end, ]]
     theme = "wave",              -- Load "wave" theme when 'background' option is not set
     background = {               -- map the value of 'background' option to a theme
         dark = "wave",           -- try "dragon" !
         light = "lotus"
     },
-      })
-    end
+      }
   },
 
   -- add gruvbox
